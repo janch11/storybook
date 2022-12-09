b1b65deab649d3a93c3aa633586647fcd641c6c2
jchoma <jandominikchoma@gmail.com>
2022-12-09 10:54:31 +0100

    add grid template and custom viewports

---
+++ .storybook/customViewports.js
@@ -0,0 +1,38 @@
+export const customViewports = {
+    xSmall: {
+      name: 'x-small',
+      styles: {
+        width: '360px',
+      },
+    },
+    small: {
+      name: 'small',
+      styles: {
+        width: '600px',
+      },
+    },
+    medium: {
+        name: 'medium',
+        styles: {
+          width: '800px',
+        },
+      },
+      medium: {
+        name: 'medium',
+        styles: {
+          width: '800px',
+        },
+      },
+      large: {
+        name: 'large',
+        styles: {
+          width: '1024px',
+        },
+      },
+      xLarge: {
+        name: 'x-large',
+        styles: {
+          width: '1288px',
+        },
+      },
+  };
